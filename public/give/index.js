372cb769a15e758fbb92cdc954e14b3134330650
IanSSenne <IanSSenne@gmail.com>
2020-09-03 10:46:59 -0700

    remove .png from output

--- public/give/index.js
+++ public/give/index.js
@@ -1,2 +1,2 @@
 const fs = require("fs");
-fs.writeFileSync("./output.json", JSON.stringify(fs.readdirSync("./item").filter(_ => _.endsWith(".png")).map(_ => ({ id: _, name: _.replace(/_/g, " ") }))));
+fs.writeFileSync("./output.json", JSON.stringify(fs.readdirSync("./item").filter(_ => _.endsWith(".png")).map(_ => ({ id: _.replace(".png", ""), name: _.replace(/_/g, " ").replace(".png", "") }))));
